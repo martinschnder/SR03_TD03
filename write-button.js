087836a7b440f7b0bc06bf8fcd37ec77e5b59caa
oceane <oceane.bordeau@etu.utc.fr>
2022-04-01 16:14:16 +0200

    chat

---
+++ write-button.js
@@ -0,0 +1,5 @@
+function write() {
+    console.log("button");
+    // document.getElementsByClassName("write-button").style.visibility = "hidden";
+    // document.getElementsByClassName("form").style.display="visible";
+}
